c72f254c6a9fff139228bb01c50a09dda91712d9
the company <thecompany001@protonmail.com>
2021-12-21 23:20:06 -0500

    Create room join page

--- api/views.py
+++ api/views.py
@@ -29,6 +29,21 @@ class GetRoom(APIView):
     
         return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
 
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(this.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
