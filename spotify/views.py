899becd6b842b393c5bc500ec9162802015e132b
the company <thecompany001@protonmail.com>
2021-12-28 11:46:00 -0500

    Spotify API (auth & tokens)

--- spotify/views.py
+++ spotify/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-
+from .util import update_or_create_user_tokens
 
    
 
@@ -40,3 +40,10 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('frontend:')
