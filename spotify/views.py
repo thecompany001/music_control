7fc04a5ace73a0dcddb74a294ad38e1a99e76896
the company <thecompany001@protonmail.com>
2021-12-27 20:49:49 -0500

    Spotify API (auth & tokens)

--- spotify/views.py
+++ spotify/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
 from rest_framework.views import APIView
-from requests Request, post
+from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+
+
+   
+
 
 class AuthURL(APIView):
     def get(self, request, format=None):
@@ -19,5 +23,20 @@ class AuthURL(APIView):
 
         return Response({'url': url}, status=status.HTTP_200_OK)
 
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
 
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }).json()
 
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
