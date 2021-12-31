b56824eb71da754c94bc93478be318c13da45d3c
the company <thecompany001@protonmail.com>
2021-12-30 22:29:25 -0500

    skip song

--- spotify/util.py
+++ spotify/util.py
@@ -75,7 +75,10 @@ def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
 
 
 def play_song(session_id):
-    return execute_spotify_api_request(session_id, "player/play", put_=True
+    return execute_spotify_api_request(session_id, "player/play", put_=True)
 
 def pause_song(session_id):
-    return execute_spotify_api_request(session_id, "player/pause", put_=True
+    return execute_spotify_api_request(session_id, "player/pause", put_=True)
+
+def skip_song(session_id:
+    return execute_spotify_api_request(session_id, "player/next", post_=True)
